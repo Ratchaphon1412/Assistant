210bf09e48e1c80d43b3508b3567502b324d0191
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2022-06-11 20:39:44 +0700

    edit text to speech

--- TextToSpeech/textToSpeech.py
+++ TextToSpeech/textToSpeech.py
@@ -16,8 +16,9 @@ class textTTS:
             'Content-Type' : 'application/json'
             }
         payload ={
-            "voice":"th-TH-NiwatNeural",
-            "content":text,
+            'voice':'th-TH-NiwatNeural',
+            'content':text,
+            'title': 'test'
         }
         
         response = requests.post("https://play.ht/api/v1/convert",data=json.dumps(payload),headers=headers)
@@ -36,17 +37,17 @@ class textTTS:
             if dicresponseConvert["converted"]:
                 urldownloadMP3 = dicresponseConvert["audioUrl"]
                 mp3 = requests.get(urldownloadMP3,allow_redirects=True)
-                open('Voice.mp3','wb').write(mp3.content) 
-                playsound('Voice.mp3')
-                os.remove('Voice.mp3')
+                open('Voice.wav','wb').write(mp3.content) 
+                playsound('Voice.wav')
+                os.remove('Voice.wav')
             else:
                 return
         else:
             language = 'th'
             voice = gTTS(text=text, lang=language, slow=False)
-            voice.save('Voice.mp3')
-            playsound('Voice.mp3')
-            os.remove('Voice.mp3')
+            voice.save('Voice.wav')
+            playsound('Voice.wav')
+            os.remove('Voice.wav')
             return
         
         
