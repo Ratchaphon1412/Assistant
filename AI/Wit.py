78d4df257308065fd457fc4cc26be0bbd02b4cbe
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2022-06-15 01:21:14 +0700

    fix error check intent empty

--- AI/Wit.py
+++ AI/Wit.py
@@ -13,10 +13,16 @@ class Wit:
         }
         response = requests.get(self.endpointMessage+text,headers=headers)
         dicresponse = json.loads(response.text)
-        
-        intent = dicresponse["intents"][0]["name"] 
-        confidence = dicresponse["intents"][0]["confidence"] 
-        entities = dicresponse["entities"]
+
+        intent = None
+        confidence = None
+        entities = None
+        if(dicresponse["intents"]!= list() ):
+            intent = dicresponse["intents"][0]["name"] 
+            confidence = dicresponse["intents"][0]["confidence"]
+        if(dicresponse["entities"] != dict()):
+            entities = dicresponse["entities"]
+     
         
 
         return intent,confidence,entities
