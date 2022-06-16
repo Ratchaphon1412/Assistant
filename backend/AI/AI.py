bf0cadca50db2c4e6513a6acd37d1cac17385363
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2022-06-16 13:35:10 +0700

    create directory backend

---
+++ backend/AI/AI.py
@@ -0,0 +1,45 @@
+from SpeechToText.speechToText import SpeechTT
+from TextToSpeech.textToSpeech import textTTS
+from Knowledge.main import Knowlegde
+from .Wit import Wit
+import asyncio
+
+class AI:
+    def __init__(self,weatherAPI,playhtHeader,witAPI,rapidAPI):
+        self.speechTT = SpeechTT()
+        self.textTTS = textTTS(playhtHeader)
+        self.knowLedge = Knowlegde(weatherAPI,rapidAPI)
+        self.wit = Wit(witAPI)
+
+    def mainAI(self):
+
+        while(True):
+            try:
+                audio,recognition = self.speechTT.startListen()
+                text = self.speechTT.changevoiceTT(audio,recognition)
+                print(text)
+                checkConvertPlayht = None
+                if text != None:
+                    intent,confidence,entities= self.wit.getIntent(text)
+                    if(intent != None):
+                        if(confidence >= 0.85):
+                            if(intent == "greeting"):
+                                asyncio.run(self.textTTS.changetextTV("สวัสดีครับ"))
+                            if(intent == "weather"):
+                               text = self.knowLedge.weather(entities)
+                               checkConvertPlayht = asyncio.run(self.textTTS.main(text))
+                               self.checkConvertPlayht(checkConvertPlayht)
+                        else:
+                            self.textTTS.dontunderstand()
+                    else:
+                        self.textTTS.nofeature()
+            except:
+                break
+    
+    def checkConvertPlayht(self,check):
+        if check[1] == False:
+            self.textTTS.problemPlayht()
+        return
+                
+
+
---
+++ backend/Knowledge/nlg.py
@@ -0,0 +1,29 @@
+from fileinput import close
+import random
+class Nlg:
+    def __init__(self):
+        pass
+    def answerWeather(self,des,temp):
+        mergeString = ["มี"," สภาพอากาศที่คุณอยู่ มี","ณ ที่คุณอยู่ มี"]
+        speechtemp = [""," อุณหภูมิประมาณ " + temp + "องศา"]
+        speech = None
+        if "ฝน" in des:
+            speech = mergeString[random.randrange(0,2)] + des + speechtemp[random.randrange(0,1)] + "อย่าลืมพกร่มไปด้วยนะครับ"
+        else:
+            speech = mergeString[random.randrange(0,2)] + des + speechtemp[random.randrange(0,1)] + "ครับ"
+        print(speech)
+        return speech
+    
+    def answerPlaceWeather(self,des,temp,text):
+        mergeString = ["จากที่ทราบข้อมูล แถวๆ บริเวณ ","บริเวณ ","จากข้อมูลพบว่า แถวนั้น "]
+        desMerge = ["มี","สภาพอากาศ มี"]
+        speechtemp = [""," อุณหภูมิประมาณ " + temp + "องศา"]
+        
+        if "ฝน" in des:
+            speech = mergeString[random.randrange(0,2)] + text  + desMerge[random.randrange(0,1)] + des + speechtemp[random.randrange(0,1)] + "อย่าลืมพกร่มไปด้วยนะครับ"
+        else:
+            speech = mergeString[random.randrange(0,2)] + text  + desMerge[random.randrange(0,1)] + des + speechtemp[random.randrange(0,1)] + "ครับ"
+        
+        print(speech)
+        return speech
+
