eb5b5812c9497e3fe147701fe0a3cb54143cdce6
Ratchaphon1412 <90958561+Ratchaphon1412@users.noreply.github.com>
2022-06-19 12:42:13 +0700

    delete node module

--- docs/node_modules/photoswipe/dist/types/ui/button-zoom.d.ts
+++
@@ -1,3 +0,0 @@
-export default zoomButton;
-/** @type {import("./ui-element").UIElementData} UIElementData */
-declare const zoomButton: import("./ui-element").UIElementData;
--- docs/node_modules/photoswipe/dist/types/ui/counter-indicator.d.ts
+++
@@ -1,2 +0,0 @@
-/** @type {import("./ui-element").UIElementData} UIElementData */
-export const counterIndicator: import("./ui-element").UIElementData;
