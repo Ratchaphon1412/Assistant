d52d5eaa86028bec1f26fafacff094d70cbde255
Ratchaphon Hinsui <ratchaphonhinsui@Ratchaphons-MacBook-Pro.local>
2022-06-30 12:21:30 +0700

    edit setup.sh

--- backend/setup.sh
+++ backend/setup.sh
@@ -2,6 +2,6 @@
 
 brew install portaudio
 python3 -m venv AssistantEnv
-source AssistantsEnv/bin/activate
+source AssistantEnv/bin/activate
 pip3 install -r requirements.txt
 pip3 install --global-option='build_ext' --global-option='-I/usr/local/include' --global-option='-L/usr/local/lib' PyAudio
