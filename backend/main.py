239fabae48e8ec513194d633803ffdf9bcaffa22
Ratchaphon Hinsui <ratchaphonhinsui@Ratchaphons-MacBook-Pro.local>
2022-07-18 17:12:18 +0700

    use flask socket

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,24 @@
+from crypt import methods
 from AI.AI import AI
 import json
+from flask import Flask
+from flask_socketio import SocketIO
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'AISecretKey'
+socketio = SocketIO(app)
+
+
+@app.route("/")
+def getWeather():
+    return "<p>Test</p>"
 
 
 if __name__ == '__main__':
+
+    socketio.run(app)
+
     with open('key.json')as fileJson:
         key = json.load(fileJson)
         weatherAPI = key["APIKEY"]["weatherAPI"]
